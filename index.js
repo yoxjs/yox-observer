d9cd80583a7529d7bb1ec3118ab302caaf005ee3
musicode <musicode@qq.com>
2018-01-29 17:30:21 +0800

    update

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ function updateValue(changes, newValue, oldValue, keypath) {
 function eachKeypath(keypath, callback) {
   if (callback(keypath) !== env.FALSE) {
     for (let i = keypath.length - 1; i >= 0;) {
-      i = keypath.lastIndexOf('.', i)
+      i = keypath.lastIndexOf(env.KEYPATH_SEPARATOR, i)
       if (i > 0) {
         if (callback(keypath.substr(0, i)) === env.FALSE) {
           return
